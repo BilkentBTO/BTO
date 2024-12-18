af13783c8b0e1905914a5c18350b159475d74f57
Egorto04 <ege.ertem@ug.bilkent.edu.tr>
2024-12-18 14:53:35 +0300

    [Frontend] View reg is fixed

--- App/frontend/src/ViewRegistrationPage/ViewRegistrationPage.jsx
+++ App/frontend/src/ViewRegistrationPage/ViewRegistrationPage.jsx
@@ -30,61 +30,67 @@ function ViewRegistrationPage() {
   }, []);
 
   return (
-    <div className="viewRegistrationPage">
+    <div>
       <HeaderGlobal name={"YOUR TOUR REGISTRATION"} />
-      <div className="viewRegistrationInfo">
-        {/* Registration Information */}
-        <p className="viewRegistrationCode">
-          <span className="viewLabel">Registration Code:</span>{" "}
-          {registrationCode || "N/A"}
-        </p>
-        <p className="viewSchoolName">
-          <span className="viewLabel">School Name:</span> {schoolName || "N/A"}
-        </p>
-        <p className="viewCity">
-          <span className="viewLabel">City:</span> {city || "N/A"}
-        </p>
-        <p className="viewVisitDate">
-          <span className="viewLabel">Date of Visit:</span>{" "}
-          {dateOfVisit ? new Date(dateOfVisit).toLocaleDateString() : "N/A"}
-        </p>
-        <p className="viewPreferedDate">
-          <span className="viewLabel">Preferred Time of Visit:</span>{" "}
-          {prefferedVisitTime?.startTime
-            ? new Date(prefferedVisitTime.startTime).toLocaleTimeString()
-            : "N/A"}
-        </p>
-        <p className="viewVisitorNum">
-          <span className="viewLabel">Number of Visitors:</span>{" "}
-          {numberOfVisitors || "N/A"}
-        </p>
-        <p className="viewSupervisor">
-          <span className="viewLabel">Supervisor Name:</span>{" "}
-          {superVisorName || "N/A"}
-        </p>
-        <p className="viewSupervisorDuty">
-          <span className="viewLabel">Supervisor Duty:</span>{" "}
-          {superVisorDuty || "N/A"}
-        </p>
-        <p className="viewSupervisorPhoneNum">
-          <span className="viewLabel">Supervisor Phone Number:</span>{" "}
-          {superVisorPhoneNumber || "N/A"}
-        </p>
-        <p className="viewSupervisorMail">
-          <span className="viewLabel">Supervisor Mail:</span>{" "}
-          {superVisorMailAddress || "N/A"}
-        </p>
-        <p className="viewNotes">
-          <span className="viewLabel">Notes:</span> {notes || "N/A"}
-        </p>
+      <div className="viewRegistrationPage">
+        <div className="viewRegistrationInfo">
+          {/* Registration Information */}
+          <p className="viewRegistrationCode">
+            <span className="viewLabel">Registration Code:</span>{" "}
+            {registrationCode || "N/A"}
+          </p>
+          <p className="viewSchoolName">
+            <span className="viewLabel">School Name:</span>{" "}
+            {schoolName || "N/A"}
+          </p>
+          <p className="viewCity">
+            <span className="viewLabel">City:</span> {city || "N/A"}
+          </p>
+          <p className="viewVisitDate">
+            <span className="viewLabel">Date of Visit:</span>{" "}
+            {dateOfVisit ? new Date(dateOfVisit).toLocaleDateString() : "N/A"}
+          </p>
+          <p className="viewPreferedDate">
+            <span className="viewLabel">Preferred Time of Visit:</span>{" "}
+            {prefferedVisitTime?.startTime
+              ? new Date(prefferedVisitTime.startTime).toLocaleTimeString()
+              : "N/A"}
+          </p>
+          <p className="viewVisitorNum">
+            <span className="viewLabel">Number of Visitors:</span>{" "}
+            {numberOfVisitors || "N/A"}
+          </p>
+          <p className="viewSupervisor">
+            <span className="viewLabel">Supervisor Name:</span>{" "}
+            {superVisorName || "N/A"}
+          </p>
+          <p className="viewSupervisorDuty">
+            <span className="viewLabel">Supervisor Duty:</span>{" "}
+            {superVisorDuty || "N/A"}
+          </p>
+          <p className="viewSupervisorPhoneNum">
+            <span className="viewLabel">Supervisor Phone Number:</span>{" "}
+            {superVisorPhoneNumber || "N/A"}
+          </p>
+          <p className="viewSupervisorMail">
+            <span className="viewLabel">Supervisor Mail:</span>{" "}
+            {superVisorMailAddress || "N/A"}
+          </p>
+          <p className="viewNotes">
+            <span className="viewLabel">Notes:</span> {notes || "N/A"}
+          </p>
+          <p className="viewSupervisorMail">
+            <span className="viewLabel">State:</span> Approved
+          </p>
 
-        {/* Buttons */}
-        <div className="viewButtonSection">
-          <ButtonHeaderGlobal
-            name={"Cancel Registration"}
-            link="/"
-          ></ButtonHeaderGlobal>
-          <ButtonHeaderGlobal name={"Back"} link="/"></ButtonHeaderGlobal>
+          {/* Buttons */}
+          <div className="viewButtonSection">
+            <ButtonHeaderGlobal
+              name={"Cancel Registration"}
+              link="/"
+            ></ButtonHeaderGlobal>
+            <ButtonHeaderGlobal name={"Back"} link="/"></ButtonHeaderGlobal>
+          </div>
         </div>
       </div>
     </div>
