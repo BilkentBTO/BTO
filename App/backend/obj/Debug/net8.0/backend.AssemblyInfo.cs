5d46d01cfbc80e25501b48fc2cf56f6d9e96f0e3
KTNVS <krmcdk@outlook.com>
2024-12-20 18:44:18 +0300

    [Backend] Added MailJet Library

--- App/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ App/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4abb02ccc9fc8b46c79324266772f923196e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ee53ed58edc5a71a5220ed75c7eb8d91cf6db0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
