1b5d272195c438a178fdb01c8c4acb4a49273a78
Can Kütükoğlu <cankutukoglu03@gmail.com>
2024-12-23 19:00:12 +0300

    [Frontend] DataPanel finished

--- App/frontend/src/GlobalClasses/ColumnGraphGlobal.jsx
+++ App/frontend/src/GlobalClasses/ColumnGraphGlobal.jsx
@@ -8,15 +8,18 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  LabelList,
 } from "recharts";
 
-function ColumnGraphGlobal(data, width, height) {
-  const CustomToolTip = ({ active, payload, label }) => {
+function ColumnGraphGlobal({ data, width, height }) {
+  const CustomToolTip = ({ active, payload }) => {
     if (active && payload && payload.length) {
       return (
         <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
-          <p className="text-medium text-lg">{label}</p>
-          <p className="text-sm text-blue-400">
+          <p className="text-medium text-lg" style={{ color: "black" }}>
+            {payload[0].payload.name}
+          </p>
+          <p className="text-sm" style={{ color: "black" }}>
             Value: <span className="ml-2">${payload[0].value}</span>
           </p>
         </div>
@@ -26,15 +29,22 @@ function ColumnGraphGlobal(data, width, height) {
   };
 
   return (
-    <div className="chart-container" style={{ width: width, height: height }}>
+    <div className="chart-container" style={{ width, height }}>
       <ResponsiveContainer>
         <BarChart data={data} margin={{ right: 30 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="name" tick={false} /> {/* Hide X-axis labels */}
           <YAxis />
           <Tooltip content={<CustomToolTip />} />
           <Legend />
-          <Bar dataKey="value" fill="#2563eb" />
+          <Bar dataKey="value" fill="#7070c2">
+            {/* Add LabelList for text directly on bars */}
+            <LabelList
+              dataKey="value"
+              position="top"
+              style={{ fill: "#3c3c82" }}
+            />
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
